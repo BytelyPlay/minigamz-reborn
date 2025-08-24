917ada20992b3ce6893ed9826605d0d5538d5fab
BytelyPlay <universalnebula2024@outlook.com>
2025-08-24 11:25:48 +0200

    Fabric part now works

--- random-items-minigame/src/main/java/org/minigamzreborn/bytelyplay/randomItems/Main.java
+++ random-items-minigame/src/main/java/org/minigamzreborn/bytelyplay/randomItems/Main.java
@@ -1,10 +1,12 @@
 package org.minigamzreborn.bytelyplay.randomItems;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.MinecraftServer;
 
 public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
 rootProject.name = "MinigamzReborn"
 
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+
+
 include("common")
 include("hub")
-// include("random-items-minigame")
+include("random-items-minigame")
