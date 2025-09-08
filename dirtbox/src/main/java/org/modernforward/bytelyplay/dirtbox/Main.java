ad0e4fa22202eb2f3cb4edeee2a52d9026d3a4da
BytelyPlay <universalnebula2024@outlook.com>
2025-09-08 09:36:23 +0200

    I have added the protocol implementation to dirtbox.

--- dirtbox/src/main/java/org/modernforward/bytelyplay/dirtbox/Main.java
+++ dirtbox/src/main/java/org/modernforward/bytelyplay/dirtbox/Main.java
@@ -5,6 +5,10 @@ import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.InstanceManager;
+import org.minigamzreborn.bytelyplay.protobuffer.packets.RegisterServerPacketC2SOuterClass;
+import org.minigamzreborn.bytelyplay.protobuffer.packets.WrappedPacketC2SOuterClass;
+import org.minigamzreborn.bytelyplay.protocol.ProtocolMain;
+import org.minigamzreborn.bytelyplay.protocol.utils.Server;
 import org.modernforward.bytelyplay.dirtbox.listeners.PlayerJoinHandlers;
 import org.modernforward.bytelyplay.dirtbox.utils.Instances;
 
@@ -12,7 +16,11 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class Main {
+    public Server protocolServer;
     public static void main(String[] args) {
+        new Main();
+    }
+    public Main() {
         MinecraftServer server = MinecraftServer.init();
 
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
@@ -20,12 +28,23 @@ public class Main {
         Instances.dirtbox = instanceManager.createInstanceContainer();
 
         setupEvents();
+        setupProtocol();
 
         // TODO: Make this configurable.
         server.start(new InetSocketAddress("0.0.0.0", 25569));
     }
-    private static void setupEvents() {
+    private void setupEvents() {
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, PlayerJoinHandlers::asyncPlayerConfigEvent);
+    }
+    private void setupProtocol() {
+        protocolServer = ProtocolMain.initClient("127.0.0.1", 9485);
+
+        protocolServer.sendPacket(WrappedPacketC2SOuterClass.WrappedPacketC2S.newBuilder()
+                .setRegisterServerPacket(RegisterServerPacketC2SOuterClass.RegisterServerPacketC2S.newBuilder()
+                        .setAddress("127.0.0.1")
+                        .setPort(25569)
+                        .build())
+                .build());
     }
 }
